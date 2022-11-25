6dd0d16e4419fa690ace8d96a988614f1a0083c0
junnun15 <junnunprodhan@gmail.com>
2022-11-26 00:13:12 +0600

    dynamic route added

--- index.js
+++ index.js
@@ -28,51 +28,55 @@ async function run(){
     
         app.post('/users',async(req,res)=>{
             const users = req.body;
-            console.log(users)
-            const UserResult =await userCollection.insertOne(users)
-            res.send(UserResult)
+            console.log(users);
+            const UserResult =await userCollection.insertOne(users);
+            res.send(UserResult);
         })
         app.get('/users',async(req,res)=>{
             const users = req.body;
-            console.log(users)
-            const UserResult =await userCollection.insertOne(users)
-            res.send(UserResult)
+            console.log(users);
+            const UserResult =await userCollection.insertOne(users);
+            res.send(UserResult);
         })
 
 
         const CategoryCollection = client.db('CarMarket').collection('category');
         app.post('/category', async(req, res) => {
             const category = req.body;
-            const result= await CategoryCollection.insertOne(category)
+            const result= await CategoryCollection.insertOne(category);
             res.send(result); 
         })
 
 
         app.get('/category', async(req, res)=>{
             const query={}
-            const Cursor=CategoryCollection.find(query)
-            const category=await Cursor.toArray()
-            res.send(category)
+            const Cursor=CategoryCollection.find(query);
+            const category=await Cursor.toArray();
+            res.send(category);
         })
 
 
         const ProductCollection = client.db('CarMarket').collection('Products');
         app.post('/product', async(req, res) => {
             const product = req.body;
-            const result= await ProductCollection.insertOne(product)
+            const result= await ProductCollection.insertOne(product);
             res.send(result); 
         })
 
         app.get('/product', async(req, res)=>{
             const query={}
-            const Cursor=ProductCollection.find(query)
-            const product=await Cursor.toArray()
+            const Cursor=ProductCollection.find(query);
+            const product=await Cursor.toArray();
+            res.send(product);
+        })
+
+        app.get('/product/:id', async(req, res)=>{
+            const id =req.params.id;
+            const serQuery={category:id}
+            const productCursor=ProductCollection.find(serQuery)
+            const product=await productCursor.toArray()
             res.send(product)
         })
-        
-
-
-    
     }
     finally{
     
