2b21687df4a3367a1573eaae0041c807300b1eeb
junnun15 <junnunprodhan@gmail.com>
2022-11-25 18:39:51 +0600

    add product collection

--- index.js
+++ index.js
@@ -22,7 +22,48 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
+async function run(){
+    try{
+        const userCollection = client.db('CarMarket').collection('users');
+    
+        app.post('/users',async(req,res)=>{
+            const users = req.body;
+            console.log(users)
+            const UserResult =await userCollection.insertOne(users)
+            res.send(UserResult)
+        })
+        app.get('/users',async(req,res)=>{
+            const users = req.body;
+            console.log(users)
+            const UserResult =await userCollection.insertOne(users)
+            res.send(UserResult)
+        })
 
+
+        const CategoryCollection = client.db('CarMarket').collection('category');
+        app.post('/category', async(req, res) => {
+            const category = req.body;
+            const result= await CategoryCollection.insertOne(category)
+            res.send(result); 
+        })
+
+        
+        const ProductCollection = client.db('CarMarket').collection('Products');
+        app.post('/product', async(req, res) => {
+            const product = req.body;
+            const result= await ProductCollection.insertOne(product)
+            res.send(result); 
+        })
+
+
+    
+    }
+    finally{
+    
+    }
+    
+    }
+    run().catch(err=>console.error(err))
 
 
 
